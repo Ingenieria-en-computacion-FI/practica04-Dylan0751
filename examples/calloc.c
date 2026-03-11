28f0c0f6d0a7ccaa550aaaf36de81da732949d01
Dylan Osvaldo <dylan.osvaldo.garcia@gmail.com>
2026-03-11 17:53:52 -0600

    primer commit

--- examples/calloc.c
+++ examples/calloc.c
@@ -3,18 +3,20 @@
 
 
 int main (){
-  // TODO(1): Declara un apuntador a entero llamado array
-  // TODO(2): Declara una variable para indicar cuantos elementos se requieren
+  int *array;
+  int num2;
   
   printf("¿Cuantos elementos tiene el conjunto?\n");
-  // TODO(3): Lee el numero de elementos
-  // TODO(4): Usando una llamada a calloc reserva la memoria para el array
+  scanf("%d", &num2);
+  array = (int*)calloc(num2, sizeof(int));
   
   if (array!=NULL) {
     printf("[ ");
-    // TODO(5): Usando un ciclo for imprime la información en el arreglo
+    for(int i = 0; i < num2; i++){
+      printf("%d", *(array+i));
+    }
     printf("]\n");
-    // TODO(6): libera la memoria reservada por malloc
+    free(array);
   }
   return 0;
 }
